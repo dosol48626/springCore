dd46590dc07156741da9432ea26e6d378d7de0e1
DESKTOP-RNOAK4B\admin <dosol48626@naver.com>
2024-10-07 16:37:01 +0900

    1007 수업자료

---
+++ src/main/java/com/dosol/springcore/mapper/TimeMapper.java
@@ -0,0 +1,11 @@
+package com.dosol.springcore.mapper;
+
+import org.apache.ibatis.annotations.Select;
+
+public interface TimeMapper {
+    @Select("select now()")
+    String getTime();
+
+    @Select("select count(*) from test_tbl")
+    int getCount();
+}
